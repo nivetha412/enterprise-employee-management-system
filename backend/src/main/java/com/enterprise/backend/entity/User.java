3587afa90271d34d5acd909535fbed15e4fc80b4
nivetha412 <gnivetha412@gmail.com>
2026-06-11 18:41:11 +0530

    repo created

---
+++ backend/src/main/java/com/enterprise/backend/entity/User.java
@@ -0,0 +1,39 @@
+package com.enterprise.backend.entity;
+
+import com.enterprise.backend.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    @Builder.Default
+     private Boolean active = true;
+}
